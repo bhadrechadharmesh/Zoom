4d45c64405853a3a3d951320edce2ff24a64c14e
bhadrechadharmesh <bhadrechadharmesh141@gmail.com>
2026-01-24 23:37:17 +0530

    day 1

--- backend/app.js
+++ backend/app.js
@@ -3,5 +3,32 @@ import {createServer} from "node:http"
 
 import {Server} from "socket.io"
 
-import mongoose from "mongoose"
+import mongoose, { mongo } from "mongoose"
+import { connectToSocket } from "./controllers/socketManager.js"
+import cors from "cors"
+
+const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
+
+app.set("port",(process.env.PORT || 8000))
+
+app.set("mongoUrl",(process.env.MONGO_URL || "your_url"));
+
+app.use(cors());
+app.use(express.json({limit:"40kb"}))
+app.use(express.urlencoded({limit:"40kb" , extended:true}))
+
+
+const start = async ()=>{
+    // const connectionDb = await mongoose.connect("mongoUrl")
+
+    // console.log(`mongo connected Host: ${connectionDb.connection.host}`);
+    server.listen(app.get("port"),()=>{
+        console.log("server started");
+    })
+}
+
+start()
+
 
