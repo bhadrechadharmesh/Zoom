8280efb258e1bf3075debeb9b9290e5401facb57
bhadrechadharmesh <bhadrechadharmesh141@gmail.com>
2026-01-25 23:21:48 +0530

    routes and controllers setup

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,14 @@
 import express from "express"
 import {createServer} from "node:http"
+import dotenv from "dotenv";
 
-import {Server} from "socket.io"
-
-import mongoose, { mongo } from "mongoose"
+import mongoose from "mongoose"
 import { connectToSocket } from "./controllers/socketManager.js"
 import cors from "cors"
+
+import userRoute from "./routes/users_routes.js";
+
+dotenv.config();
 
 const app = express();
 const server = createServer(app);
@@ -13,17 +16,16 @@ const io = connectToSocket(server);
 
 app.set("port",(process.env.PORT || 8000))
 
-app.set("mongoUrl",(process.env.MONGO_URL || "your_url"));
-
 app.use(cors());
 app.use(express.json({limit:"40kb"}))
 app.use(express.urlencoded({limit:"40kb" , extended:true}))
 
+app.use("/api/v1/users",userRoute);
 
 const start = async ()=>{
-    // const connectionDb = await mongoose.connect("mongoUrl")
+    const connectionDb = await mongoose.connect(process.env.MONGO_URI)
 
-    // console.log(`mongo connected Host: ${connectionDb.connection.host}`);
+    console.log(`mongo connected Host: ${connectionDb.connection.host}`);
     server.listen(app.get("port"),()=>{
         console.log("server started");
     })
